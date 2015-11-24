4577756baca5f7a2cee8b63b9a9f24f23196ddab
jason mayer <jason.mayer@gmail.com>
2015-11-24 11:08:44 -0600

    changing rails root and env to rails 3  methods

--- init.rb
+++ init.rb
@@ -1,4 +1,5 @@
-if RAILS_ENV =~ /^test/ || RAILS_ENV =~ /selenium/
+#if RAILS_ENV =~ /^test/ || RAILS_ENV =~ /selenium/
+if Rails.env.test? || Rails.env.selenium? #RAILS_ENV =~ /^test/ || RAILS_ENV =~ /selenium/
   require 'mainline'
   require 'mainline/connection_pool'
   require 'mainline/integration_test'
--- lib/mainline/server.rb
+++ lib/mainline/server.rb
@@ -3,24 +3,24 @@ module Mainline
     def initialize(config)
       @config = config
     end
-    
+
     def start
       options = {:Port => @config.port, :Host => "0.0.0.0"}
-      
+
       @server = Rack::Handler::WEBrick
-      require RAILS_ROOT + "/config/environment"
+      require Rails.root + "/config/environment"
       inner_app = ActionController::Dispatcher.new
       app = Rack::Builder.new {
         use Rails::Rack::Static
         run inner_app
       }.to_app
-#      
-#      trap(:INT) do 
+#
+#      trap(:INT) do
 #        @server.stop
 #        Kernel.exit
 #        thread.join
 #      end
-      
+
       @server.run(app, options.merge(:AccessLog => []))
     end
   end
